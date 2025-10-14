22302f7eef14c0b2f4d4773080c7e3e775a28c55
Selina Liu <selinaliu@MacBook-Air-8.local>
2025-10-14 12:52:57 -0400

    cors and frontend thru backend

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
 using backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,26 +15,36 @@ builder.Services.AddHttpClient();
 builder.Services.AddScoped<WeatherService>();
 
 // CORS, for development purpose only, remove for final submission
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("DevCorsPolicy", builder =>
-    {
-        builder.WithOrigins("http://localhost:5173")
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
-});
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("DevCorsPolicy", builder =>
+//     {
+//         builder.WithOrigins("http://localhost:5173")
+//                .AllowAnyMethod()
+//                .AllowAnyHeader();
+//     });
+// });
 
 var app = builder.Build();
 
-// Warning: This is used to avoid CORS, it basically runs the frontend built code
-// app.UseDefaultFiles();
-// app.UseStaticFiles();
+// Warning: This is used to avoid CORS, it serves frontend through backend
+var frontendPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "frontend", "dist");
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    FileProvider = new PhysicalFileProvider(frontendPath),
+    RequestPath = ""
+});
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(frontendPath),
+    RequestPath = ""
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseCors("DevCorsPolicy");
+    // app.UseCors("DevCorsPolicy");
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
